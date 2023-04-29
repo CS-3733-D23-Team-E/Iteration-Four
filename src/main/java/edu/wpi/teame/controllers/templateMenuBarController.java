6d7c5e780150b4188de10485555e8e9834d8c3f9
arvirone <arvirone@wpi.edu>
2023-04-29 17:48:42 -0400

    fixed bug

--- src/main/java/edu/wpi/teame/controllers/templateMenuBarController.java
+++ src/main/java/edu/wpi/teame/controllers/templateMenuBarController.java
@@ -9,104 +9,100 @@ import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 
-public class templateMenuBarController
-{
-    @FXML MFXButton menuBarHome;
-    @FXML MFXButton menuBarServices;
-    @FXML MFXButton menuBarMaps;
-    @FXML MFXButton menuBarAbout;
-    @FXML MFXButton menuBarDatabase;
-    @FXML MFXButton menuBarSignage;
-    @FXML MFXButton menuBarBlank;
-    @FXML MFXButton menuBarExit;
-    @FXML VBox menuBar;
-    @FXML VBox logoutBox;
-    @FXML MFXButton logoutButton;
-    @FXML MFXButton userButton;
-    @FXML ImageView homeI;
-    @FXML ImageView aboutI;
-    @FXML ImageView servicesI;
-    @FXML ImageView signageI;
-    @FXML ImageView pathfindingI;
-    @FXML ImageView databaseI;
-    @FXML ImageView settingsI;
-    @FXML ImageView exitI;
-    @FXML MFXButton menuBarSettings;
-    Boolean loggedIn;
-    String language = "english";
-    boolean menuVisibilty = false;
-    boolean logoutVisible = false;
+public class templateMenuBarController {
+  @FXML MFXButton menuBarHome;
+  @FXML MFXButton menuBarServices;
+  @FXML MFXButton menuBarMaps;
+  @FXML MFXButton menuBarAbout;
+  @FXML MFXButton menuBarDatabase;
+  @FXML MFXButton menuBarSignage;
+  @FXML MFXButton menuBarBlank;
+  @FXML MFXButton menuBarExit;
+  @FXML VBox menuBar;
+  @FXML VBox logoutBox;
+  @FXML MFXButton logoutButton;
+  @FXML MFXButton userButton;
+  @FXML ImageView homeI;
+  @FXML ImageView aboutI;
+  @FXML ImageView servicesI;
+  @FXML ImageView signageI;
+  @FXML ImageView pathfindingI;
+  @FXML ImageView databaseI;
+  @FXML ImageView settingsI;
+  @FXML ImageView exitI;
+  @FXML MFXButton menuBarSettings;
+  Boolean loggedIn;
+  String language = "english";
+  boolean menuVisibilty = false;
+  boolean logoutVisible = false;
 
-    String nyay = "\u00F1"; // ñ
-    String aA = "\u0301"; // á
-    String aE = "\u00E9"; // é
-    String aI = "\u00ED"; // í
-    String aO = "\u00F3"; // ó
-    String aU = "\u00FA"; // ù
-    String aQuestion = "\u00BF"; // Upside down question mark
+  String nyay = "\u00F1"; // ï¿½
+  String aA = "\u0301"; // ï¿½
+  String aE = "\u00E9"; // ï¿½
+  String aI = "\u00ED"; // ï¿½
+  String aO = "\u00F3"; // ï¿½
+  String aU = "\u00FA"; // ï¿½
+  String aQuestion = "\u00BF"; // Upside down question mark
 
-    public void initialize()
-    {
-        //Menu Bar Navigation code
-        menuBarSignage.setOnMouseClicked(event -> Navigation.navigate(Screen.SIGNAGE_EDITOR_PAGE));
-        menuBarServices.setOnMouseClicked(event -> Navigation.navigate(Screen.SERVICE_REQUESTS));
-        menuBarHome.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
-        menuBarMaps.setOnMouseClicked(event -> Navigation.navigate(Screen.MAP));
-        menuBarAbout.setOnMouseClicked(event -> Navigation.navigate((Screen.ABOUT)));
-        menuBarSettings.setOnMouseClicked(event -> Navigation.navigate(Screen.SETTINGSPAGE));
-        menuBarDatabase.setOnMouseClicked(event -> Navigation.navigate((Screen.DATABASE_TABLEVIEW)));
-        menuBarExit.setOnMouseClicked(event -> Platform.exit());
+  public void initialize() {
+    // Menu Bar Navigation code
+    menuBarSignage.setOnMouseClicked(event -> Navigation.navigate(Screen.SIGNAGE_EDITOR_PAGE));
+    menuBarServices.setOnMouseClicked(event -> Navigation.navigate(Screen.SERVICE_REQUESTS));
+    menuBarHome.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
+    menuBarMaps.setOnMouseClicked(event -> Navigation.navigate(Screen.MAP));
+    menuBarAbout.setOnMouseClicked(event -> Navigation.navigate((Screen.ABOUT)));
+    menuBarSettings.setOnMouseClicked(event -> Navigation.navigate(Screen.SETTINGSPAGE));
+    menuBarDatabase.setOnMouseClicked(event -> Navigation.navigate((Screen.DATABASE_TABLEVIEW)));
+    menuBarExit.setOnMouseClicked(event -> Platform.exit());
 
-        // makes the menu bar buttons get highlighted when the mouse hovers over them
-        ButtonUtilities.mouseSetupMenuBar(
-                menuBarHome,
-                "baseline-left",
-                homeI,
-                "images/house-blank.png",
-                "images/house-blank-blue.png");
-        ButtonUtilities.mouseSetupMenuBar(
-                menuBarServices,
-                "baseline-left",
-                servicesI,
-                "images/hand-holding-medical.png",
-                "images/hand-holding-medical-blue.png");
-        ButtonUtilities.mouseSetupMenuBar(
-                menuBarSignage,
-                "baseline-left",
-                signageI,
-                "images/diamond-turn-right.png",
-                "images/diamond-turn-right-blue.png");
-        ButtonUtilities.mouseSetupMenuBar(
-                menuBarMaps, "baseline-left", pathfindingI, "images/marker.png", "images/marker-blue.png");
-        ButtonUtilities.mouseSetupMenuBar(
-                menuBarDatabase,
-                "baseline-left",
-                databaseI,
-                "images/folder-tree.png",
-                "images/folder-tree-blue.png");
-        ButtonUtilities.mouseSetupMenuBar(
-                menuBarAbout, "baseline-left", aboutI, "images/abouticon.png", "images/abouticon-blue.png");
-        ButtonUtilities.mouseSetupMenuBar(
-                menuBarSettings,
-                "baseline-left",
-                settingsI,
-                "images/settingsicon.png",
-                "images/settingsicon-blue.png");
-        ButtonUtilities.mouseSetupMenuBar(
-                menuBarExit,
-                "baseline-center",
-                exitI,
-                "images/sign-out-alt.png",
-                "images/sign-out-alt-blue.png");
-    }
+    // makes the menu bar buttons get highlighted when the mouse hovers over them
+    ButtonUtilities.mouseSetupMenuBar(
+        menuBarHome,
+        "baseline-left",
+        homeI,
+        "images/house-blank.png",
+        "images/house-blank-blue.png");
+    ButtonUtilities.mouseSetupMenuBar(
+        menuBarServices,
+        "baseline-left",
+        servicesI,
+        "images/hand-holding-medical.png",
+        "images/hand-holding-medical-blue.png");
+    ButtonUtilities.mouseSetupMenuBar(
+        menuBarSignage,
+        "baseline-left",
+        signageI,
+        "images/diamond-turn-right.png",
+        "images/diamond-turn-right-blue.png");
+    ButtonUtilities.mouseSetupMenuBar(
+        menuBarMaps, "baseline-left", pathfindingI, "images/marker.png", "images/marker-blue.png");
+    ButtonUtilities.mouseSetupMenuBar(
+        menuBarDatabase,
+        "baseline-left",
+        databaseI,
+        "images/folder-tree.png",
+        "images/folder-tree-blue.png");
+    ButtonUtilities.mouseSetupMenuBar(
+        menuBarAbout, "baseline-left", aboutI, "images/abouticon.png", "images/abouticon-blue.png");
+    ButtonUtilities.mouseSetupMenuBar(
+        menuBarSettings,
+        "baseline-left",
+        settingsI,
+        "images/settingsicon.png",
+        "images/settingsicon-blue.png");
+    ButtonUtilities.mouseSetupMenuBar(
+        menuBarExit,
+        "baseline-center",
+        exitI,
+        "images/sign-out-alt.png",
+        "images/sign-out-alt-blue.png");
+  }
 
-    public void translateToSpanish()
-    {
-        //Add code once page is set up
-    }
+  public void translateToSpanish() {
+    // Add code once page is set up
+  }
 
-    public void translateToEnglish()
-    {
-        //Add code once page is set up
-    }
+  public void translateToEnglish() {
+    // Add code once page is set up
+  }
 }
