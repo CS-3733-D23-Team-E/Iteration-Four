9b3f1bf95b1d4afc08dd7de65b122618504b5ad3
michibatoryu <114957192+michibatoryu@users.noreply.github.com>
2023-04-29 15:45:56 -0400

    updating request status from task view now works

--- src/main/java/edu/wpi/teame/controllers/TaskViewController.java
+++ src/main/java/edu/wpi/teame/controllers/TaskViewController.java
@@ -9,8 +9,7 @@ import edu.wpi.teame.entities.ServiceRequestData;
 import java.util.List;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
+import javafx.scene.control.*;
 import javafx.scene.text.Text;
 
 public class TaskViewController {
@@ -24,10 +23,11 @@ public class TaskViewController {
   @FXML Text pendingRequestsTitleText;
   @FXML Text completedRequestTitleText;
   @FXML Label nonCompletedTitleText;
-  @FXML ListView<String> outgoingRequestsList;
+  @FXML ListView<ServiceRequestData> outgoingRequestsList;
 
   @FXML
   public void initialize() {
+    setupFactories();
     fillServiceRequestsFields();
   }
 
@@ -92,16 +92,94 @@ public class TaskViewController {
     List<ServiceRequestData> nonCompleteRequests =
         requests.stream().filter(request -> !request.getRequestStatus().equals(DONE)).toList();
 
-    List<String> requestTexts =
-        nonCompleteRequests.stream()
-            .map(
-                request ->
-                    (request.getRequestType()
-                        + " request, ID "
-                        + request.getRequestID()
-                        + ": "
-                        + request.getRequestStatus()))
-            .toList();
-    outgoingRequestsList.setItems(FXCollections.observableList(requestTexts));
+    outgoingRequestsList.setItems(FXCollections.observableList(nonCompleteRequests));
+  }
+
+  private void setupFactories() {
+    outgoingRequestsList.setCellFactory(
+        lv -> {
+          ListCell<ServiceRequestData> cell = new ListCell<>();
+          ContextMenu contextMenu = new ContextMenu();
+
+          MenuItem pathfindingItem = new MenuItem();
+          pathfindingItem.textProperty().set("View directions");
+          pathfindingItem.setOnAction(
+              event -> {
+                System.out.println("open the damn pathfinding page!");
+              });
+          MenuItem detailsItem = new MenuItem();
+          detailsItem.textProperty().set("View details");
+          detailsItem.setOnAction(
+              event -> {
+                System.out.println("open the damn service request page!");
+              });
+          MenuItem statusItem = new MenuItem();
+          statusItem
+              .textProperty()
+              .set(cell.getItem() != null ? getUpdatedStatusText(cell.getItem()) : "");
+          statusItem.setOnAction(
+              event -> {
+                System.out.println("edited the damn database!");
+                upgradeStatus(cell.getItem());
+              });
+          contextMenu.getItems().addAll(pathfindingItem, detailsItem, statusItem);
+
+          // adds the context menu to now-filled cells (if you are an admin)
+          cell.emptyProperty()
+              .addListener(
+                  (obs, wasEmpty, isNowEmpty) -> {
+                    if (!isNowEmpty) {
+                      cell.setContextMenu(contextMenu);
+                    } else {
+                      cell.setContextMenu(null);
+                    }
+                  });
+          // update the item when the item changes
+          cell.itemProperty()
+              .addListener(
+                  (event) -> {
+                    cell.textProperty()
+                        .setValue(
+                            cell.getItem() != null ? formatServiceRequest(cell.getItem()) : "");
+                    statusItem
+                        .textProperty()
+                        .set(cell.getItem() != null ? getUpdatedStatusText(cell.getItem()) : "");
+                  });
+
+          return cell;
+        });
+  }
+
+  private String formatServiceRequest(ServiceRequestData request) {
+    return request.getRequestType()
+        + " request, ID "
+        + request.getRequestID()
+        + ": "
+        + request.getRequestStatus();
+  }
+
+  private String getUpdatedStatusText(ServiceRequestData request) {
+    switch (request.getRequestStatus()) {
+      case PENDING:
+        return "Switch status to IN_PROGRESS";
+      case IN_PROGRESS:
+        return "Switch status to DONE";
+      case DONE:
+        return "Delete Request";
+      default:
+        return "";
+    }
+  }
+
+  private void upgradeStatus(ServiceRequestData request) {
+    switch (request.getRequestStatus()) {
+      case PENDING:
+        SQLRepo.INSTANCE.updateServiceRequest(request, "status", "IN_PROGRESS");
+      case IN_PROGRESS:
+        SQLRepo.INSTANCE.updateServiceRequest(request, "status", "DONE");
+      case DONE:
+        SQLRepo.INSTANCE.deleteServiceRequest(request);
+    }
+    fillServiceRequestsFields();
   }
 }
