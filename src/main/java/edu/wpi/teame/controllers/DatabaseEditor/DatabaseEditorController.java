0bd571adcf5aab6d289f9d4d9f0d960c9c8b55b6
Albert Lewis <alberttheperson@gmail.com>
2023-05-01 16:02:28 -0400

    redid some of database

--- src/main/java/edu/wpi/teame/controllers/DatabaseEditor/DatabaseEditorController.java
+++ src/main/java/edu/wpi/teame/controllers/DatabaseEditor/DatabaseEditorController.java
@@ -1,64 +1,33 @@
 package edu.wpi.teame.controllers.DatabaseEditor;
 
 import edu.wpi.teame.entities.Settings;
-import edu.wpi.teame.utilities.ButtonUtilities;
 import edu.wpi.teame.utilities.Navigation;
 import edu.wpi.teame.utilities.Screen;
-import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 
 public class DatabaseEditorController {
 
-  @FXML DatabaseMapViewController mapViewController;
+  //  @FXML DatabaseMapViewController mapViewController;
   @FXML DatabaseTableViewController tableViewController;
-  @FXML DatabaseServiceRequestViewController serviceRequestViewController;
-  @FXML MoveComponentController moveComponentController;
-
-  @FXML AnchorPane tableView;
-  @FXML AnchorPane moveView;
-  @FXML AnchorPane serviceView;
-  @FXML AnchorPane mapView;
-  @FXML MFXButton menuButton;
-  @FXML MFXButton menuBarHome;
-  @FXML MFXButton menuBarServices;
-  @FXML MFXButton menuBarMaps;
-
-  @FXML MFXButton menuBarAbout;
-  @FXML MFXButton menuBarDatabase;
-
-  @FXML MFXButton menuBarSignage;
-  @FXML MFXButton menuBarBlank;
-  @FXML MFXButton menuBarExit;
-  @FXML VBox menuBar;
-  @FXML AnchorPane employeeView;
-
-  @FXML MFXButton tableEditorSwapButton;
-  @FXML MFXButton mapEditorSwapButton;
-  @FXML MFXButton moveEditorSwapButton;
-  @FXML MFXButton requestsEditorSwapButton;
-  @FXML MFXButton employeeEditorSwapButton;
+  //  @FXML DatabaseServiceRequestViewController serviceRequestViewController;
+  //  @FXML MoveComponentController moveComponentController;
+  @FXML ToggleGroup editorGroup;
+  @FXML ToggleButton tableEditorSwapButton;
+  @FXML ToggleButton mapEditorSwapButton;
+  @FXML ToggleButton moveEditorSwapButton;
+  @FXML ToggleButton requestsEditorSwapButton;
+  @FXML ToggleButton employeeEditorSwapButton;
   @FXML VBox importExportZone;
-
-  @FXML MFXButton importButton;
-  @FXML MFXButton exportButton;
-
-  @FXML MFXButton backButton;
+  @FXML Button importButton;
+  @FXML Button exportButton;
+  @FXML Button backButton;
   @FXML Label editorTitle;
-  @FXML ImageView aboutI;
-  @FXML ImageView servicesI;
-  @FXML ImageView signageI;
-  @FXML ImageView pathfindingI;
-  @FXML ImageView databaseI;
-  @FXML ImageView settingsI;
-  @FXML ImageView exitI;
-  @FXML ImageView homeI;
-
-  Boolean menuVisibilty = false;
 
   String nyay = "\u00F1"; // �
   String aA = "\u0301"; // �
@@ -70,66 +39,21 @@ public class DatabaseEditorController {
 
   @FXML
   public void initialize() {
-    //    onlyVisible(tableView);
+
+    editorGroup
+        .selectedToggleProperty()
+        .addListener(
+            ((observable, oldToggle, newToggle) -> {
+              if (newToggle == null) {
+                oldToggle.setSelected(true);
+                return;
+              }
+
+              importExportZone.setVisible(newToggle.equals(tableEditorSwapButton));
+            }));
+
     importExportZone.setVisible(true);
-    //    onlyDisable(tableEditorSwapButton);
     editorTitle.setText("Table editor");
-    // Initially set the menu bar to invisible
-    menuBarVisible(false);
-
-    // When the menu button is clicked, invert the value of menuVisibility and set the menu bar to
-    // that value
-    // (so each time the menu button is clicked it changes the visibility of menu bar back and
-    // forth)
-    menuButton.setOnMouseClicked(
-        event -> {
-          menuVisibilty = !menuVisibilty;
-          menuBarVisible(menuVisibilty);
-        });
-
-    // Navigation controls for the button in the menu bar
-    menuBarHome.setOnMouseClicked(
-        event -> {
-          Navigation.navigate(Screen.HOME);
-          menuVisibilty = !menuVisibilty;
-        });
-
-    // makes the menu bar buttons get highlighted when the mouse hovers over them
-    ButtonUtilities.mouseSetupMenuBar(
-        menuBarHome,
-        "baseline-left",
-        homeI,
-        "images/house-blank.png",
-        "images/house-blank-blue.png");
-    ButtonUtilities.mouseSetupMenuBar(
-        menuBarServices,
-        "baseline-left",
-        servicesI,
-        "images/hand-holding-medical.png",
-        "images/hand-holding-medical-blue.png");
-    ButtonUtilities.mouseSetupMenuBar(
-        menuBarSignage,
-        "baseline-left",
-        signageI,
-        "images/diamond-turn-right.png",
-        "images/diamond-turn-right-blue.png");
-    ButtonUtilities.mouseSetupMenuBar(
-        menuBarMaps, "baseline-left", pathfindingI, "images/marker.png", "images/marker-blue.png");
-    ButtonUtilities.mouseSetupMenuBar(
-        menuBarDatabase,
-        "baseline-left",
-        databaseI,
-        "images/folder-tree.png",
-        "images/folder-tree-blue.png");
-    ButtonUtilities.mouseSetupMenuBar(
-        menuBarAbout, "baseline-left", aboutI, "images/abouticon.png", "images/abouticon-blue.png");
-
-    ButtonUtilities.mouseSetupMenuBar(
-        menuBarExit,
-        "baseline-center",
-        exitI,
-        "images/sign-out-alt.png",
-        "images/sign-out-alt-blue.png");
 
     initButtons();
 
@@ -170,28 +94,6 @@ public class DatabaseEditorController {
     exportButton.setOnAction(event -> tableViewController.exportTable());
 
     backButton.setOnAction(event -> Navigation.navigate(Screen.HOME));
-  }
-
-  private void onlyDisable(MFXButton btn) {
-    tableEditorSwapButton.setDisable(false);
-    mapEditorSwapButton.setDisable(false);
-    moveEditorSwapButton.setDisable(false);
-    requestsEditorSwapButton.setDisable(false);
-    employeeEditorSwapButton.setDisable(false);
-    btn.setDisable(true);
-  }
-
-  private void onlyVisible(AnchorPane pane) {
-    tableView.setVisible(false);
-    moveView.setVisible(false);
-    serviceView.setVisible(false);
-    mapView.setVisible(false);
-    employeeView.setVisible(false);
-    pane.setVisible(true);
-  }
-
-  public void menuBarVisible(boolean bool) {
-    menuBar.setVisible(bool);
   }
 
   public void translateToSpanish() {
