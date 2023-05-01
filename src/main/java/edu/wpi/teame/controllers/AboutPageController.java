be2d04953e34f700635bc3ade95fb7af812ec085
Megan Jacques <mjacques@wpi.edu>
2023-04-30 22:20:27 -0400

    Added menu bars to about page

--- src/main/java/edu/wpi/teame/controllers/AboutPageController.java
+++ src/main/java/edu/wpi/teame/controllers/AboutPageController.java
@@ -2,12 +2,10 @@ package edu.wpi.teame.controllers;
 
 import static javafx.scene.paint.Color.WHITE;
 
-import edu.wpi.teame.utilities.ButtonUtilities;
 import edu.wpi.teame.utilities.Navigation;
 import edu.wpi.teame.utilities.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import java.awt.*;
-import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
@@ -90,74 +88,8 @@ public class AboutPageController {
     creditsButton.setOnMouseClicked(event -> Navigation.navigate((Screen.CREDITS)));
 
     // Initially set the menu bar to invisible
-    menuBarVisible(false);
+
     logoutPopup(false);
-
-    // When the menu button is clicked, invert the value of menuVisibility and set the menu bar to
-    // that value
-    // (so each time the menu button is clicked it changes the visibility of menu bar back and
-    // forth)
-    menuButton.setOnMouseClicked(
-        event -> {
-          menuVisibilty = !menuVisibilty;
-          menuBarVisible(menuVisibilty);
-        });
-
-    userButton.setOnMouseClicked(
-        event -> {
-          logoutVisible = !logoutVisible;
-          logoutPopup(logoutVisible);
-        });
-
-    // Navigation controls for the button in the menu bar
-    menuBarHome.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
-    menuBarServices.setOnMouseClicked(
-        event -> {
-          Navigation.navigate(Screen.SERVICE_REQUESTS);
-          menuVisibilty = !menuVisibilty;
-        });
-    menuBarSignage.setOnMouseClicked(event -> Navigation.navigate(Screen.SIGNAGE_EDITOR_PAGE));
-    menuBarMaps.setOnMouseClicked(event -> Navigation.navigate(Screen.MAP));
-    menuBarDatabase.setOnMouseClicked(event -> Navigation.navigate(Screen.DATABASE_EDITOR));
-    menuBarAbout.setOnMouseClicked(event -> Navigation.navigate(Screen.ABOUT));
-    menuBarExit.setOnMouseClicked((event -> Platform.exit()));
-    logoutButton.setOnMouseClicked(event -> Navigation.navigate(Screen.SIGNAGE_TEXT));
-
-    // makes the menu bar buttons get highlighted when the mouse hovers over them
-    ButtonUtilities.mouseSetupMenuBar(
-        menuBarHome,
-        "baseline-left",
-        homeI,
-        "images/house-blank.png",
-        "images/house-blank-blue.png");
-    ButtonUtilities.mouseSetupMenuBar(
-        menuBarServices,
-        "baseline-left",
-        servicesI,
-        "images/hand-holding-medical.png",
-        "images/hand-holding-medical-blue.png");
-    ButtonUtilities.mouseSetupMenuBar(
-        menuBarSignage,
-        "baseline-left",
-        signageI,
-        "images/diamond-turn-right.png",
-        "images/diamond-turn-right-blue.png");
-    ButtonUtilities.mouseSetupMenuBar(
-        menuBarMaps, "baseline-left", pathfindingI, "images/marker.png", "images/marker-blue.png");
-    ButtonUtilities.mouseSetupMenuBar(
-        menuBarDatabase,
-        "baseline-left",
-        databaseI,
-        "images/folder-tree.png",
-        "images/folder-tree-blue.png");
-    ButtonUtilities.mouseSetupMenuBar(
-        menuBarAbout, "baseline-left", aboutI, "images/abouticon.png", "images/abouticon-blue.png");
-    ButtonUtilities.mouseSetupMenuBar(
-        menuBarExit,
-        "baseline-center",
-        exitI,
-        "images/sign-out-alt.png",
-        "images/sign-out-alt-blue.png");
 
     mouseSetup(logoutButton);
 
@@ -220,18 +152,6 @@ public class AboutPageController {
 
   public void logoutPopup(boolean bool) {
     logoutBox.setVisible(bool);
-  }
-
-  public void menuBarVisible(boolean bool) {
-    menuBarHome.setVisible(bool);
-    menuBarServices.setVisible(bool);
-    menuBarSignage.setVisible(bool);
-    menuBarMaps.setVisible(bool);
-    menuBarDatabase.setVisible(bool);
-    menuBarAbout.setVisible(bool);
-    menuBarBlank.setVisible(bool);
-    menuBarExit.setVisible(bool);
-    menuBar.setVisible(bool);
   }
 
   private void mouseSetup(MFXButton btn) {
