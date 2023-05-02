4ce3cf1efca49b3dc9b43ad4b33b2eb3510d859b
Albert Lewis <alberttheperson@gmail.com>
2023-05-02 00:31:56 -0400

    created fxml variable

--- src/main/java/edu/wpi/teame/controllers/SignageController.java
+++ src/main/java/edu/wpi/teame/controllers/SignageController.java
@@ -2,6 +2,7 @@ package edu.wpi.teame.controllers;
 
 import edu.wpi.teame.Database.SQLRepo;
 import edu.wpi.teame.entities.Employee;
+import edu.wpi.teame.entities.SignageComponentData;
 import edu.wpi.teame.utilities.Navigation;
 import edu.wpi.teame.utilities.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
@@ -10,15 +11,20 @@ import java.awt.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
+
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.input.KeyCode;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.util.Duration;
+
+import javax.swing.text.html.ImageView;
 
 public class SignageController {
   @FXML MFXButton userButton;
@@ -33,11 +39,40 @@ public class SignageController {
   @FXML Text timeText;
   @FXML Text dateText;
 
-  // TODO: Make login work
+  @FXML
+    ImageView arrowOneRight;
+    @FXML
+    ImageView arrowTwoRight;
+    @FXML
+    ImageView arrowThreeRight;
+    @FXML
+    ImageView arrowFourRight;
+    @FXML
+    ImageView arrowOneLeft;
+    @FXML
+    ImageView arrowTwoLeft;
+    @FXML
+    ImageView arrowThreeLeft;
+    @FXML
+    ImageView arrowFourLeft;
+
+    @FXML Label labelOneRight;
+    @FXML Label labelTwoRight;
+    @FXML Label labelThreeRight;
+    @FXML Label labelFourRight;
+    @FXML Label labelOneLeft;
+    @FXML Label labelTwoLeft;
+    @FXML Label labelThreeLeft;
+    @FXML Label labelFourLeft;
+
+
+    // TODO: Make login work
 
   boolean loginVisible = false;
 
   public void initialize() {
+
+    List<SignageComponentData> listOfSignage = SQLRepo.INSTANCE.getSignageList();
 
     loginPopout(false);
     loginFailBox.setVisible(false);
@@ -88,6 +123,12 @@ public class SignageController {
 
     timeline.setCycleCount(Animation.INDEFINITE);
     timeline.play();
+
+    populateSignage();
+  }
+
+  private void populateSignage() {
+
   }
 
   private void attemptLogin() {
